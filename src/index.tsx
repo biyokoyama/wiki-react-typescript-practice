3fe53e2688aa22e04a39d4b66dd07be81c0cc2aa
biyokoyama <biyokoyamajp@gmail.com>
2021-01-16 20:02:54 +0900

    fontawesome導入

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import "./index/fontawesome.ts";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
