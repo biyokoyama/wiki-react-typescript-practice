a8c8d12a200079a69b743fad0304843b4098d931
biyokoyama <biyokoyamajp@gmail.com>
2021-01-30 15:27:52 +0900

    redux-devtools-extension導入

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,11 @@
 import { combineReducers, createStore } from "redux";
 import { searchFormReducer } from "./searchForm";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export const rootReducer = combineReducers({
   searchForm: searchFormReducer,
 });
 export type RootState = ReturnType<typeof rootReducer>;
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 export default store;
